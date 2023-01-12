c65b3ca3507abde5012379e4e6550289e298b095
Felexonyango <felexonyango19@gmail.com>
2023-01-12 23:09:35 +0300

    update repo

---
+++ fizzbuzz.js
@@ -0,0 +1,20 @@
+function FizzBuzz(n){
+
+    for(let i=1; i<=n; i++){
+  
+        if(i % 3 === 0 && i % 5){
+            console.log("fizzBuzz")
+        }
+        else if(i%3===0){
+            console.log('Fizz')
+        }
+        else if(i%5===0){
+            console.log('Buzz')
+        } 
+        else{
+            console.log(i)
+        }
+
+    }
+}
+console.log(FizzBuzz(20))
