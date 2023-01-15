174a5996bb1ddb2073651ce54aea99267b9d5154
Felexonyango <felexonyango19@gmail.com>
2023-01-15 11:23:27 +0300

    LengthofLastword

---
+++ LengthOfLastWord.js
@@ -0,0 +1,18 @@
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
+
+ 
+function lengthOfLastWord(s){
+    let word=s.trim().split(/\s+/)
+    // trim() method to remove any leading or trailing whitespaces from the input string. 
+    // Then it splits the string using
+    //  a regular expression that matches one or more whitespace characters /\s+/
+
+    let lastword =word[word.length-1]
+    return lastword.length
+ 
+}; 
+console.log(lengthOfLastWord('hello yes'))
