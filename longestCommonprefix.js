12c86c69e472b0043540026b2e258288d096bf51
Felexonyango <felexonyango19@gmail.com>
2023-01-15 15:14:45 +0300

    changes on long

--- longestCommonprefix.js
+++ longestCommonprefix.js
@@ -1,17 +1,12 @@
-// Write a function to find the longest common prefix string amongst an array of strings.
-// If there is no common prefix, return an empty string "".
 
-function findlongestPrefix(str){
-    for(let i=0;i<str.length; i++){
-        let ascending=str.sort((a,b)=>b.length-a.length)
-        let shortest =ascending[0]
-        while(!str.every((x)=>x.startsWith(shortest))){
-            shortest = shortest.slice(0, -1);
-        }
-        return shortest;
+function findlongestPrefixs(str) {
+  for (let i = 0; i < str.length; i++) {
+    let ascending = str.sort((a, b) => b.length - a.length);
+    let shortest = ascending[0];
+    while (!str.every((x) => x.startsWith(shortest))) {
+      shortest= shortest.slice(0, -1);
     }
-
-
-
+    return  shortest
+  }
 }
-console.log( findlongestPrefix(["hello","Hi","heee"]))
+console.log(findlongestPrefixs(["flower", "flow", "flight"]));
