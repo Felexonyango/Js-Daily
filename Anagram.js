b6b220ef50196a1296d3cb83e3c25f71bf9ee6b3
Felexonyango <felexonyango19@gmail.com>
2023-01-18 20:36:14 +0300

    anagram solution

--- Anagram.js
+++ Anagram.js
@@ -1,40 +1,39 @@
 function checkIfAnagram(s1, s2) {
-    let a=s1.split('').sort().join()
-    let b=s2.split('').sort().join()
+  let a = s1.split("").sort().join();
+  let b = s2.split("").sort().join();
 
-    a===b ? console.log('true'): console.log('false')
- 
-  }
-  console.log(checkIfAnagram('test','tes'))
-
+  a === b ? console.log("true") : console.log("false");
+}
+console.log(checkIfAnagram("test", "tes"));
 
 
 
-
-
-
-
-   
-  function groupAnagrams(words) {
+function GroupAnagram(words) {
     const anagrams = {};
-    for (const word of words) {
-        const sortedWord = word.split('').sort().join('');
-        if (anagrams[sortedWord]) {
-            anagrams[sortedWord].push(word);
-        } else {
-            anagrams[sortedWord] = [word];
-        }
+    for (let i = 0; i < words.length; i++) {
+      const sortedWord = words[i].split('').sort().join('');
+      if (anagrams[sortedWord]) {
+          anagrams[sortedWord].push(words[i])
+      }
+      else{
+          anagrams[sortedWord] = [words[i]]
+      }
     }
     return Object.values(anagrams);
+  }
+  console.log(GroupAnagram(["eat", "tea", "tan", "ate", "nat", "bat"]));
+  
+
+
+function GroupAnagrams(words) {
+  const anagrams = {}; // to store all the anagram groups
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+    anagrams[sortedWord]
+      ? anagrams[sortedWord].push(word)
+      : (anagrams[sortedWord] = [word]);
+  }
+  return Object.values(anagrams)
 }
+console.log(GroupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
 
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
-
-// Create an empty object, anagrams, to store all the anagram groups.
-// Loop through the input list of words.
-// For each word, sort the characters alphabetically and 
-//join them together to form a new string. 
-//This new string represents a unique "signature" for all anagrams of that word.
-// If a group already exists in the anagrams object for the current
-// "signature" with the current word as the first member.
-// Return the values of anagrams object using object.values()
--- coins.js
+++ coins.js
@@ -1,21 +1,26 @@
-function makeChange(V, denominations) {
-    let result = {};  // stores the number of coins  of each denominator
+// Given a value of V Rs and an infinite supply of each of the denominations
+// {1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, 
+//The task is to find the minimum number of coins and/or notes needed to make the change?
+
+
+function DenomoinationChnage(V, denominations) {
+    let res = {};  // stores the number of coins  of each denominator
     let i =0;
     while(i<denominations.length){
         let coin = denominations[i];
-        result[coin] = Math.floor(V / coin);
+        res[coin] = Math.floor(V / coin);
         V = V % coin;
         i++
     }
-    return result;
-    // for (let i = 0; i < denominations.length; i++) {
-    //     let coin = denominations[i];
-    //     result[coin] = Math.floor(V / coin);
-    //     V = V % coin;
-    // }
-    // return result;
+    return res;
 }
 
-let V = 470;
-let denominations = [1000, 500, 100, 50, 20, 10, 5, 2, 1];
-console.log(makeChange(V, denominations));
+console.log(DenomoinationChnage(70, [1000, 500, 100, 50, 20, 10, 5, 2, 1]));
+
+
+// for (let i = 0; i < denominations.length; i++) {
+//     let coin = denominations[i];
+//     result[coin] = Math.floor(V / coin);
+//     V = V % coin;
+// }
+// return result;
