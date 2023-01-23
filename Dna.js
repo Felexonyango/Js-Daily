1353dcef5e19ac86e9b992f697e865dad3d50e70
Felexonyango <felexonyango19@gmail.com>
2023-01-23 15:15:16 +0300

    updated Dna quiz

--- Dna.js
+++ Dna.js
@@ -1,17 +1,38 @@
 function findRepeatedDnaSequences(s) {
-  let emptyArry = [];
+  let emptyObject = {};
+  let result = [];
   for (let i = 0; i <= s.length - 10; i++) {
-    let substring = s.slice(i, i + 10);
-    if (!emptyArry.includes(substring)) {
-      emptyArry.push(substring);
-    }
-  } 
-  return emptyArry.filter((substring) =>emptyArry.indexOf(substring) !== emptyArry.lastIndexOf(substring)
-  );
+      let substring = s.substring(i, i + 10);
+      if (emptyObject[substring]) {
+        if(emptyObject[substring]===1){
+          result.push(substring);
+
+        }
+        emptyObject[substring] += 1;
+      }
+      else {
+          emptyObject[substring] = 1;
+      }
+  }
+  return result;
 }
-console.log(findRepeatedDnaSequences("ACGAATTCCG"));
+console.log(findRepeatedDnaSequences("ACGACGACGACGACGACGACG"));
 
-// Hashmap  practise
-
-
-
+function findRepeatedDnaSequencess(s) {
+  let map = new Map();
+  let result = [];
+  for (let i = 0; i <= s.length - 10; i++) {
+      let substring = s.substring(i, i + 10);
+      if (map.has(substring)) {
+          if(map.get(substring) === 1){
+              result.push(substring);
+          }
+          map.set(substring, map.get(substring) + 1);
+      } else {
+          map.set(substring, 1);
+      }
+  }
+  return result;
+  
+}
+console.log(findRepeatedDnaSequencess("ACGACGACGACGACGACGACG"));
