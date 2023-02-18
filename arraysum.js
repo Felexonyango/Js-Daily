366718a1a9bdd7efadbae7911258c38d78ff46b1
Felexonyango <felexonyango19@gmail.com>
2023-02-18 11:11:17 +0300

    added array sum using reduce method

--- arraysum.js
+++ arraysum.js
@@ -1,10 +1,14 @@
-function arraySum(n){
-    let sum =0;
-    for(let i =0; i < n.length; i++){
-        sum =sum+n[i]
-
-    }
-    return sum
-
+function arraySum(n) {
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum = sum + n[i];
+  }
+  return sum;
 }
-console.log(arraySum([1,2,3,4,5,6]))
+console.log(arraySum([1, 2, 3, 4, 5, 6]));
+
+function arraySums(n) {
+  let result = n.reduce((sum, current) => sum + current, 0);
+  return result;
+}
+console.log(arraySums([1, 2, 3, 4, 5, 6]));
