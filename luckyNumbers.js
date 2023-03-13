032a624871f71999c6ed35882bc91994f396d7e5
Felexonyango <felexonyango19@gmail.com>
2023-03-13 15:29:13 +0300

    added luckyNumber solution

---
+++ luckyNumbers.js
@@ -0,0 +1,27 @@
+// Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+// A lucky number is an element of the matrix such that it is the minimum element 
+// in its row and maximum in its column.
+var luckyNumbers  = function(matrix) {
+    let result =[];
+    for(let i=0; i<matrix.length; i++) {
+      const row  =matrix[i]
+      const min=Math.min(...row) //returning all small set in an array
+      const collIndex =row.indexOf(min) //returns index of first occurrence of min Number in a row
+      let isMax=true
+      for(let j=0;j<matrix.length;j++){
+        if(matrix[j][collIndex]>min){
+            isMax=false
+            break
+        }
+      }
+      if(isMax){
+        result.push(min)
+
+      }
+
+      
+    }
+    return result
+}
+
+console.log(luckyNumbers([[3,7,8],[9,11,13],[15,16,17]]))
