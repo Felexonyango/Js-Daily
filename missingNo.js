10ddbf89409a70d0b421496dce48583bffc016e0
Felexonyango <felexonyango19@gmail.com>
2023-04-09 23:32:40 +0300

    missing number

--- missingNo.js
+++ missingNo.js
@@ -1,12 +1,9 @@
 
-var missingNumber = function (nums) {
-    var sum = 0;
-    for (let i = 0; i < nums.length; i++) {
-      let expected =i+1
-      sum =expected-nums[i]
-       
-    }
-  
-    return sum;
-  };
-  console.log(missingNumber([4,6]))
+
+ 
+var missingNumber = function(nums) {
+  for(let i = 0; i <= nums.length; i++){
+      if(!nums.includes(i))
+          return i;
+  }
+};
