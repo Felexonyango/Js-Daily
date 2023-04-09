ff6df2928dedcedfb6e429f8c38da5f564dca0cf
Felexonyango <felexonyango19@gmail.com>
2023-04-09 23:42:11 +0300

    added smallest lettr challenge

---
+++ Letter.js
@@ -0,0 +1,10 @@
+// You are given an array of characters letters that is sorted in non-decreasing order,
+// and a character target. There are at least two different characters in letters.
+
+// Return the smallest character in letters that is lexicographically greater than target. 
+//If such a character does not exist, return the first character in letters.
+
+var nextGreatestLetter = function(letters, target) {
+    return letters.find((letter)=>letter >target)|| letters[0]
+};
+ 
